63c2d35d65df301dc9a3c9e5d06d7e8ff3befaa8
lightcici <lightcici@gmail.com>
2017-01-16 14:53:49 +0800

    Add some structure

---
+++ App_Code/Work.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Work
+/// </summary>
+public class Work
+{
+    public Work()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
