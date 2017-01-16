755f435f260a6b763547fafac8925a9a8dd71da0
lightcici <lightcici@gmail.com>
2017-01-16 23:37:35 +0800

    Login

---
+++ Login.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Image ID="Image1" runat="server" ImageUrl="~/Image/User Filled.png" />
+        <asp:Login ID="Login1" runat="server" OnAuthenticate="Login1_Authenticate">
+        </asp:Login>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+    {
+        if (Membership.ValidateUser(Login1.UserName, Login1.Password))
+        {
+            Session["UserName"] = Login1.UserName;
+            FormsAuthentication.SetAuthCookie(Login1.UserName, false);
+            Response.Redirect("");
+
+        }
+    }
+}
