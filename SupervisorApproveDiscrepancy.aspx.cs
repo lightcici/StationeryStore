e346fe1b695e476a94ada2e742bb528a5f446bd8
pan-pan1101 <837282696@qq.com>
2017-01-20 15:50:15 +0800

    PAN

--- SupervisorApproveDiscrepancy.aspx.cs
+++ SupervisorApproveDiscrepancy.aspx.cs
@@ -6,16 +6,12 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Windows.Forms;
 
-public partial class ApproveDiscrepancy : System.Web.UI.Page
+public partial class SupervisorApproveDiscrepancy : System.Web.UI.Page
 {
     Work work;
     protected void Page_Load(object sender, EventArgs e)
     {
         string userId = (string)Session["user"];
-        if (userId == null)
-        {
-            Response.Redirect("~/login.aspx");
-        }
         work = new Work();
         if (work.getSpecificDiscrepancies().Count == 0)
         {
@@ -31,7 +27,7 @@ public partial class ApproveDiscrepancy : System.Web.UI.Page
             DialogResult dr = MessageBox.Show("Successful Operation!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Question);
             if (dr == DialogResult.OK)
             {
-                Response.Redirect("/ManagerApproveDiscrepancy.aspx");
+                Response.Redirect("SupervisorApproveDiscrepancy.aspx");
             }
         }
     }
