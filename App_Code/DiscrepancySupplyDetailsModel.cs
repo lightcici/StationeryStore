65a1dfb0d27090b72d48a8f1e107787aff7c1889
pan-pan1101 <837282696@qq.com>
2017-01-23 15:35:07 +0800

    PAN PAN

---
+++ App_Code/DiscrepancySupplyDetailsModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for DiscrepancySupplyDetailsModel
+/// </summary>
+public class DiscrepancySupplyDetailsModel
+{
+    private string discrepancyId;
+    private string itemCode;
+    private string description;
+    private int quantity;
+    private string reason;
+    private Decimal price;
+
+    public DiscrepancySupplyDetailsModel()
+    {
+
+    }
+    public string DiscrepancyId
+    {
+        get { return discrepancyId; }
+        set { discrepancyId = value; }
+    }
+    public string ItemCode
+    {
+        get { return itemCode; }
+        set { itemCode = value; }
+    }
+    public string Description
+    {
+        get { return description; }
+        set { description = value; }
+    }
+    public int Quantity
+    {
+        get { return quantity; }
+        set { quantity = value; }
+    }
+    public string Reason
+    {
+        get { return reason; }
+        set { reason = value; }
+    }
+    public Decimal Price
+    {
+        get { return price; }
+        set { price = value; }
+    }
+}
